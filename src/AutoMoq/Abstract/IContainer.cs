2429c15c7a6f3268a096f55d9b3bf5dff387b623
Mikhail Podolskiy <edge769@yandex.ru>
2020-03-17 00:05:55 +0300

    add an interface for DI Container

---
+++ src/AutoMoq/Abstract/IContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoMoq.Abstract
+{
+    public interface IContainer
+    {
+        T Resolve<T>();
+        object Resolve(Type type);
+        void Register<T>(T instance);
+        void Register(object instance, Type type);
+    }
+}
