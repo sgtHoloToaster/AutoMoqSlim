040b8a3130b103d33ed05f8d8de087f660bb2457
Mikhail Podolskiy <edge769@yandex.ru>
2020-03-17 00:06:44 +0300

    add a template for AutoMoq

---
+++ src/AutoMoq/AutoMoq.cs
@@ -0,0 +1,34 @@
+﻿using AutoMoq.Abstract;
+using Moq;
+using System;
+
+namespace AutoMoq
+{
+    public class AutoMoq
+    {
+        readonly IContainer _container;
+
+        public AutoMoq(IContainer container)
+        {
+            _container = container;
+        }
+
+        public T Create<T>()
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public Mock<T> GetMock<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetInstance<T>(T instance)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
--- src/AutoMoq/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AutoMoq
-{
-    public class Class1
-    {
-    }
-}
