8c582e58f8faedc68726052a43ba93d645c27ef2
Mikhail Podolskiy <Edge769@yandex.ru>
2021-04-11 04:47:07 +0300

    upgrade tests project to .NET 5.0

---
+++ tests/AutoMoqSlim.Tests/AutoMoqSlimTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AutoMoqSlim.Tests
+{
+    public class AutoMoqSlimTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- tests/AutoMoqSlim.Tests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AutoMoqSlim.Tests
-{
-    public class Class1
-    {
-    }
-}
