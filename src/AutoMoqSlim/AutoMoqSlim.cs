09dc2c4197fee2c778874e5cbc2147c2b4d94f2a
Mikhail Podolskiy <Edge769@yandex.ru>
2021-04-11 17:34:38 +0300

    implement GetMock

--- src/AutoMoqSlim/AutoMoqSlim.cs
+++ src/AutoMoqSlim/AutoMoqSlim.cs
@@ -1,20 +1,22 @@
 ﻿using AutoMoqSlim.Abstract;
 using Moq;
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace AutoMoqSlim
 {
     public class AutoMoqSlim
     {
+        readonly ConcurrentDictionary<Type, Mock> _mocks = new();
+
         public T Create<T>()
         {
             throw new NotImplementedException();
         }
 
-        public Mock<T> GetMock<T>() where T : class
-        {
-            throw new NotImplementedException();
-        }
+        public Mock<T> GetMock<T>() where T : class =>
+            _mocks.GetOrAdd(typeof(T), new Mock<T>()).As<T>();
 
         public void SetInstance<T>(T instance)
         {
